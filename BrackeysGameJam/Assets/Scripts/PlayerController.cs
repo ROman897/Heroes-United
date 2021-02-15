577d6ff79fd0fb6f7afbd846a4a9b274b808e6e9
Loyyd <konradkunkel@gmx.de>
2021-02-15 17:12:12 +0100

    Party is able to rotate
    Just added 2 simple ways to rotate the whole Party.

--- BrackeysGameJam/Assets/Scripts/PlayerController.cs
+++ BrackeysGameJam/Assets/Scripts/PlayerController.cs
@@ -6,17 +6,27 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField]
     private float movement_speed;
+    [SerializeField]
+    private float rotation_speed;
+    [SerializeField]
+    private bool FollowsMousePos;
+    [SerializeField]
+    private bool Q_and_E_to_rotate;
 
-    private Rigidbody2D rigidbody;
+
+    private Rigidbody2D rb;
 
     public static Vector2 global_movement_dir = Vector2.zero;
 
     void Awake() {
-        rigidbody = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
+        if (FollowsMousePos) faceTowardsMouse();
+        if (Q_and_E_to_rotate) Rotation();
+
         float y_movement = 0;
         if (Input.GetKey("w")) {
             y_movement = 1;
@@ -45,7 +55,29 @@ public class PlayerController : MonoBehaviour
             moving = false;
         }
 
-        rigidbody.velocity = movement;
+        rb.velocity = movement;
         global_movement_dir = movement;
+
+        void faceTowardsMouse()
+        {
+            Vector3 mousePosition = Input.mousePosition;
+            mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+            Vector2 dir = new Vector2(mousePosition.x - transform.position.x, mousePosition.y - transform.position.y);
+
+            transform.up = dir;
+        }
+        void Rotation()
+        {
+            if (Input.GetKey("q"))
+            {
+                transform.Rotate(new Vector3(0, 0, rotation_speed )* Time.deltaTime);
+            }
+        
+            if (Input.GetKey("e"))
+            {
+                transform.Rotate(new Vector3(0, 0, -rotation_speed )* Time.deltaTime);
+            }
+        }
     }
 }
