c6a5413ff43663307367589762fa5aaa652e2162
Mimijackz <54093644+Mimijackz@users.noreply.github.com>
2021-02-16 08:13:35 +0100

    Revert "Party is able to rotate"
    This reverts commit 577d6ff79fd0fb6f7afbd846a4a9b274b808e6e9.

--- BrackeysGameJam/Assets/Scripts/PlayerController.cs
+++ BrackeysGameJam/Assets/Scripts/PlayerController.cs
@@ -6,27 +6,17 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField]
     private float movement_speed;
-    [SerializeField]
-    private float rotation_speed;
-    [SerializeField]
-    private bool FollowsMousePos;
-    [SerializeField]
-    private bool Q_and_E_to_rotate;
 
-
-    private Rigidbody2D rb;
+    private Rigidbody2D rigidbody;
 
     public static Vector2 global_movement_dir = Vector2.zero;
 
     void Awake() {
-        rb = GetComponent<Rigidbody2D>();
+        rigidbody = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        if (FollowsMousePos) faceTowardsMouse();
-        if (Q_and_E_to_rotate) Rotation();
-
         float y_movement = 0;
         if (Input.GetKey("w")) {
             y_movement = 1;
@@ -55,29 +45,7 @@ public class PlayerController : MonoBehaviour
             moving = false;
         }
 
-        rb.velocity = movement;
+        rigidbody.velocity = movement;
         global_movement_dir = movement;
-
-        void faceTowardsMouse()
-        {
-            Vector3 mousePosition = Input.mousePosition;
-            mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
-
-            Vector2 dir = new Vector2(mousePosition.x - transform.position.x, mousePosition.y - transform.position.y);
-
-            transform.up = dir;
-        }
-        void Rotation()
-        {
-            if (Input.GetKey("q"))
-            {
-                transform.Rotate(new Vector3(0, 0, rotation_speed )* Time.deltaTime);
-            }
-        
-            if (Input.GetKey("e"))
-            {
-                transform.Rotate(new Vector3(0, 0, -rotation_speed )* Time.deltaTime);
-            }
-        }
     }
 }
