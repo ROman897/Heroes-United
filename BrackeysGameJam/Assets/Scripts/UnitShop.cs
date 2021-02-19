95b04b51b306673f9d9d33dccf37490725470d6e
Mimijackz <54093644+Mimijackz@users.noreply.github.com>
2021-02-19 12:52:36 +0100

    Shop added
    right now, this is an independant shop, meaning it does not communicate with other scenes.

---
+++ BrackeysGameJam/Assets/Scripts/UnitShop.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitShop : MonoBehaviour
+{
+    public float prize;
+    public int unitNumber;
+    public string unit;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerPrefs.SetFloat("Money", 200);
+        unitNumber = UnitSlot.UnitUsed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void buyUnit()
+    {
+        if (PlayerPrefs.GetFloat("Money") >= prize && PlayerPrefs.GetString("Unit" + unitNumber) != unit)
+        {
+            Debug.Log("buy succesfull");
+            PlayerPrefs.SetFloat("Money", PlayerPrefs.GetFloat("Money") - prize);
+            PlayerPrefs.SetString("Unit" + unitNumber, unit);
+        }
+        else if (PlayerPrefs.GetFloat("Money") < prize)
+            Debug.Log("not enough money");
+        else
+            Debug.Log("already bought");
+    }
+}
---
+++ BrackeysGameJam/Assets/Scripts/UnitSlot.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+[System.Serializable]
+public class Unit
+{
+    public string name;
+    public Sprite unitSprite;
+    public GameObject unitPrefab;
+}
+public class UnitSlot : MonoBehaviour
+{
+    [SerializeField]
+    GameObject shopMenu;
+    [SerializeField]
+    int UnitNumber;
+    [SerializeField]
+    Image slotGFX;
+    [SerializeField]
+    Unit[] units;
+    bool opened = false;
+    public static int UnitUsed = 0;
+    [SerializeField]
+    UnitSlot[] slots;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!PlayerPrefs.HasKey("Unit" + UnitNumber))
+            PlayerPrefs.SetString("Unit" + UnitNumber, "");
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Unit currentUnit = getUnitByName(PlayerPrefs.GetString("Unit" + UnitNumber), units);
+        if (currentUnit != null)
+        {
+            slotGFX.sprite = currentUnit.unitSprite;
+            slotGFX.color = new Color(1, 1, 1, 1);
+        }
+        else
+            slotGFX.color = new Color(1, 1, 1, 0);
+        GetComponent<Image>().color = UnitUsed == UnitNumber ? Color.grey : Color.white;
+    }
+    public void openMenu()
+    {
+        if (UnitUsed != 0 && UnitUsed != UnitNumber)
+            slots[UnitUsed - 1].opened = false;
+        UnitUsed = opened ? 0 : UnitNumber;
+        shopMenu.SetActive(!opened);
+        opened = !opened;
+    }
+    Unit getUnitByName(string name, Unit[] unitArray)
+    {
+        foreach(Unit unit in unitArray)
+        {
+            if (unit.name == name)
+            {
+                return unit;
+            }
+        }
+        return null;
+    }
+}
